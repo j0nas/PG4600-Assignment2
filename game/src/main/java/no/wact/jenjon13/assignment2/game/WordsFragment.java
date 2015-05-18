7df24277c9e5e776482922b6ef5d54ec19059105
j0nas <jonas.jensen@msn.com>
2015-05-18 16:53:26 +0200

    Updated .gitignore and converted game to use ListView

--- game/src/main/java/no/wact/jenjon13/assignment2/game/WordsFragment.java
+++ game/src/main/java/no/wact/jenjon13/assignment2/game/WordsFragment.java
@@ -5,10 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.*;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -19,34 +16,47 @@ public class WordsFragment extends android.app.Fragment {
     private static final int WORDS_PER_TURN = 5;
     private static final int ANSWERS_PER_TURN = 3;
 
-    private List<String> activeWords;
     private String correctWord;
 
     private GameHandler gameHandler;
     private Context context;
-    private LinearLayout fragmentLayout;
+    private RelativeLayout fragmentLayout;
+    private LinearLayout controlsContain;
+    private ArrayAdapter<String> adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        fragmentLayout = (LinearLayout) rootView.findViewById(R.id.fragmentMainLayout);
+        fragmentLayout = (RelativeLayout) rootView.findViewById(R.id.fragmentMainLayout);
         context = container.getContext();
         gameHandler = new GameHandler(context);
 
-        resetUI();
+        controlsContain = (LinearLayout) fragmentLayout.findViewById(R.id.controlsContain);
+        controlsContain.addView(new NextRoundButton());
+
+        adapter = new ArrayAdapter<>(rootView.getContext(), android.R.layout.simple_list_item_1);
+        ((ListView) fragmentLayout.findViewById(R.id.wordsList)).setAdapter(adapter);
+
+        adapter.addAll(gameHandler.getNRandomWords(WORDS_PER_TURN));
+
         return rootView;
     }
 
     private void startRound() {
-        correctWord = activeWords.get(new Random().nextInt(activeWords.size()));
-        fragmentLayout.removeAllViews();
+        List<String> currentWords = new ArrayList<>();
+        for (int i = 0; i < adapter.getCount(); i++) {
+            currentWords.add(adapter.getItem(i));
+        }
 
-        Collections.shuffle(activeWords);
-        generateWordsViews(activeWords, true);
+        correctWord = currentWords.remove(new Random().nextInt(currentWords.size()));
+        Collections.shuffle(currentWords);
+        adapter.clear();
+        adapter.addAll(currentWords);
 
+        controlsContain.removeAllViews();
         // TODO: will fail if getNRandomWords gets enough words so that there aren't enough left for the answers!
-        final List<String> unusedWords = gameHandler.getUnusedWords(activeWords);
+        final List<String> unusedWords = gameHandler.getUnusedWords(currentWords);
 
         List<AnswerButton> answerButtons = new ArrayList<>();
         final int loopTo = Math.min(unusedWords.size(), ANSWERS_PER_TURN);
@@ -56,36 +66,26 @@ public class WordsFragment extends android.app.Fragment {
 
         Collections.shuffle(answerButtons);
         for (AnswerButton button : answerButtons) {
-            fragmentLayout.addView(button);
-        }
-    }
-
-    private void generateWordsViews(List<String> words, boolean hideCorrectWord) {
-        for (int i = 0; i < words.size(); i++) {
-            if (!(hideCorrectWord && words.get(i).equals(correctWord))) {
-                final TextView textView = new TextView(context);
-                textView.setText(words.get(i));
-                fragmentLayout.addView(textView);
-            }
+            controlsContain.addView(button);
         }
     }
 
     private void provideResponseToClient(String answer) {
         Toast.makeText(context, "Your answer was " + (answer.equals(correctWord) ? "" : "not ") + "correct.",
                 Toast.LENGTH_SHORT).show();
-        resetUI();
-    }
 
-    private void resetUI() {
-        fragmentLayout.removeAllViews();
-        fragmentLayout.addView(new NextRoundButton());
-        generateWordsViews(activeWords = gameHandler.getNRandomWords(WORDS_PER_TURN), false);
+        adapter.clear();
+        adapter.addAll(gameHandler.getNRandomWords(WORDS_PER_TURN));
+
+        controlsContain.removeAllViews();
+        controlsContain.addView(new NextRoundButton());
     }
 
     private class NextRoundButton extends Button {
         public NextRoundButton() {
             super(context);
             this.setText("Go!");
+            this.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
             setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -100,6 +100,7 @@ public class WordsFragment extends android.app.Fragment {
         public AnswerButton(final String label) {
             super(context);
             this.setText(label);
+            this.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
             this.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
